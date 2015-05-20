fe363cd8d105ad0cff6e71713e9af8de1cab3e4d
Pio Ryan Lumongsod <pioryan@gmail.com>
2015-05-21 01:36:13 +0800

    add db

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :scm, :git
 set :keep_releases, 5
 # Default value for :format is :pretty
 set :format, :pretty
-
+set :pg_database, "chat_live_production"
 # Default value for :log_level is :debug
 set :log_level, :debug
 
@@ -37,3 +37,49 @@ set :puma_threads, [0, 16]
 set :puma_workers, 0
 set :puma_init_active_record, true
 set :puma_preload_app, true
+
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
